77eb8be4e30973a62f4d12de128099584e27a250
Botmduser <Botdeploy698@gmail.com>
2026-06-06 14:20:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const ytdl = require("ytdl-core")
+const ytdl = require("@distube/ytdl-core")
 const ytSearch = require("yt-search")
 const cors = require("cors")
 
